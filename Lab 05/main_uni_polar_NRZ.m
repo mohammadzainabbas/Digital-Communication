07f26faf0c81be30d7e856f408608fd384cf7633
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2017-10-27 16:15:34 +0500

    Add files via upload

---
+++ Lab 05/main_uni_polar_NRZ.m
@@ -0,0 +1,22 @@
+function main_uni_polar_NRZ()
+x = [0 0 0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 0 1 1 1 1 0 0 0];
+
+Bit_rate = 5;
+Samples_per_bit_time = 200;
+Total_time = length(x)/Bit_rate;
+Bit_time = 1/Bit_rate;
+Tb = Bit_time;
+
+t = Tb/Samples_per_bit_time:Tb/Samples_per_bit_time:Total_time;
+pulse = zeros(1, (length(x)*Samples_per_bit_time));
+i = 1;
+for k = 1:Samples_per_bit_time:length(x)*Samples_per_bit_time
+    if (x(i) == 1)
+        pulse(k: k + Samples_per_bit_time - 1) = 5;
+    else
+        pulse(k: k + Samples_per_bit_time - 1) = 0;
+    end
+    i = i + 1;
+end
+plot(t, pulse);
+end
